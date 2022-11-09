c2fe2521f41eb517b057912df652b5f06b0036a3
Asbjørn <asbjorn@savvy.no>
2022-11-09 13:50:10 +0100

    rm assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: './',
   sassOptions: {
     additionalData: `
       @import "styles/variables.scss"; 
