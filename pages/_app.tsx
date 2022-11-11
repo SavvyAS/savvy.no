b476cd721848e8babcd0895dd40e6444d27d63a8
Asbjørn <asbjorn@savvy.no>
2022-11-11 13:30:04 +0100

    Added meta description for SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,20 @@ import '../node_modules/@fortawesome/fontawesome-pro/css/all.min.css'
 import '@/styles/globals.scss'
 import Layout from './layout'
 import Head from 'next/head'
+import content from '@/lib/content.json'
+import { Pages } from '@/lib/content.interface'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const { agency } = content.pages as Pages
+
   return (
     <Layout>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content={agency.ingress}
+        ></meta>
         <title>Savvy</title>
       </Head>
       <Component {...pageProps} />
