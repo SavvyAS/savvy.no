43f389af5ca147b6f3b7599e2f54f1417643c35c
Håkon Kaurel <hakon@savvy.no>
2022-11-11 16:34:18 +0100

    Generate sitemap and robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
     siteUrl: process.env.SITE_URL,
-    generateRobotsTxt: true, 
+    generateRobotsTxt: true,
+    outDir: "./out",
+    robotsTxtOptions: {
+        policies: [
+            {
+                disallow : process.env.ENV === "production" ? "" : "/",
+                userAgent: "*"
+            }
+        ]
+    }
 }
